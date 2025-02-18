3e7a6639b874ffa2647f7a6e88b36619bd153034
windvalley <levinwang6@gmail.com>
2025-02-18 18:54:50 +0800

    Nvim: update formatter

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,42 +8,39 @@ return {
         "stylua", -- default
         "shfmt", -- default
         "beautysh", -- for zsh
-        "black", -- for python
-        "isort", -- for python
-        "clang-format",
-        "goimports",
-        "goimports-reviser",
-        "golines",
-        "impl", -- for go
-        "gomodifytags",
-        "markdownlint-cli2",
-        "sql-formatter",
-        "standardjs",
-        "yamlfix",
-
-        --- ****** linter *****
-        "shellcheck",
-        "golangci-lint",
-        "nilaway", -- for go
-        "cpplint",
-        "gitlint",
-        "yamllint",
-        "stylelint", -- for css, sass, scss, less
-
-        --- ****** LSP *****
-        "clangd",
-        "gopls",
-        "golangci-lint-langserver",
-        "python-lsp-server",
-        "bash-language-server",
-        "awk-language-server",
-        "dockerfile-language-server",
-        "json-lsp",
-        "markdown-oxide", -- or remark-language-server
-        "vetur-vls", -- for vue
-        "vue-language-server",
-        "eslint-lsp",
-        "yaml-language-server",
+        -- "isort", -- for python import 合理自动分组, 会和ruff冲突，导致格式化错误，暂时注释
+        "goimports-reviser", -- Go import 合理自动分组
+        -- "golines",
+        -- "impl", -- for go
+        -- "gomodifytags",
+        -- "clang-format",
+        -- "sql-formatter",
+        -- "standardjs",
+        -- "yamlfix",
+        --
+        -- --- ****** linter *****
+        -- "shellcheck",
+        -- "golangci-lint",
+        -- "nilaway", -- for go
+        -- "cpplint",
+        -- "gitlint",
+        -- "yamllint",
+        -- "stylelint", -- for css, sass, scss, less
+        --
+        -- --- ****** LSP *****
+        -- "clangd",
+        -- "gopls",
+        -- "golangci-lint-langserver",
+        -- "python-lsp-server",
+        -- "bash-language-server",
+        -- "awk-language-server",
+        -- "dockerfile-language-server",
+        -- "json-lsp",
+        -- "markdown-oxide", -- or remark-language-server
+        -- "vetur-vls", -- for vue
+        -- "vue-language-server",
+        -- "eslint-lsp",
+        -- "yaml-language-server",
       },
     },
   },
